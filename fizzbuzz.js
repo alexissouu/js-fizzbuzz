c806d441c4f44ae3cb213e832e6231d4be67a655
alexis <alexissouchon21@gmail.com>
2022-12-20 11:29:15 +0100

    update finish

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,9 +7,33 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
+    
+ if ( x%3 === 0 && x%5 != 0) {
+    console.log("fizz")
+    return "fizz";
+ }
+
+ else if (x%5 === 0 && x%3 !=0) {
+    console.log("buzz")
+    return "buzz";
+ }
+
+ else if (x%5 === 0 && x%3 ===0) {
+    console.log("fizzbuzz")
+    return "fizzbuzz";
+ }
+
+ else {
+    console.log(x)
+    return x;
+ }
+
 };
+
+fizzbuzz(2)
+fizzbuzz(5)
+fizzbuzz(6)
+fizzbuzz(11)
+fizzbuzz(30)
 
 module.exports = { fizzbuzz: fizzbuzz };
